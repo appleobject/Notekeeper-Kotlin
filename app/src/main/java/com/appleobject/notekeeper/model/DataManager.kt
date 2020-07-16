699dfb75e2384bc4738c3a1e84d4d2dfb518dcda
appleobject <52360868+appleobject@users.noreply.github.com>
2020-07-16 10:53:22 +0100

    initial commit

---
+++ app/src/main/java/com/appleobject/notekeeper/model/DataManager.kt
@@ -0,0 +1,20 @@
+package com.appleobject.notekeeper.model
+
+class DataManager {
+    val courses = HashMap<String, CourseInfo>()
+    val notes = ArrayList<NoteInfo>()
+
+    private fun initializeCourses(){
+        var course = CourseInfo("android_intents", "Android Programming using Intents")
+        courses.set(course.courseId, course)
+
+        course = CourseInfo(courseId = "android_async", title = "Android Async Programming and Service")
+        courses.set(course.courseId, course)
+
+        course = CourseInfo(title = "Java Fundamentals: The Java Language", courseId = "java_lang")
+        courses.set(course.courseId, course)
+
+        course = CourseInfo("java_core", "Java Fundamentals: The Core Platform")
+        courses.set(course.courseId, course)
+    }
+}
---
+++ app/src/main/java/com/appleobject/notekeeper/model/NoteKeeperData.kt
@@ -0,0 +1,5 @@
+package com.appleobject.notekeeper.model
+
+class CourseInfo(val courseId: String, val title: String)
+
+class NoteInfo(var course: CourseInfo, var title: String, var text: String)
